fbb60fe777125840bf4dd7ebd13dd1604695a3a5
Firebolt1176 <Robert@20.9.16.101>
2014-05-29 08:17:33 -0700

    Modded ItemEquipable.

--- APCSGit/src/source/ItemEquipable.java
+++ APCSGit/src/source/ItemEquipable.java
@@ -4,11 +4,23 @@ package source;
  * It cannot stack
  * @author Robert Q
  */
-public class ItemEquipable extends Items {
+public abstract class ItemEquipable extends Items {
 	public EquipSlot slot; 
 	public ItemEquipable(String name,EquipSlot slot) {
 		super(name,1,1);
 		this.slot = slot;
 	}
-	public static class Helm extends ItemEquipable{public Helm(){super("Helmet",EquipSlot.HEAD);}}
+	
+	public abstract int getDefense();
+	public abstract int getAttack();
+	
+	public static class Helm extends ItemEquipable
+	{
+		public Helm (String name)
+			super(name, EquipSlot.HEAD);
+	}
+	
+	
+	public static class oneHandWeapon extends ItemEquipable{public oneHandWeapon(){super("One-handed Weapon",EquipSlot.PRIMARY);}}
+	
 }
