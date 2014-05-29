c58c6c1f5e48e7d3b85a67f2319dc86981f5a33d
Robert Quitt <robertquitt@gmail.com>
2014-05-29 09:33:33 -0700

    Updated GUI

--- APCSGit/src/source/GUI.java
+++ APCSGit/src/source/GUI.java
@@ -11,38 +11,17 @@ public class GUI{
 	public JTextField field;
 	public GUI() {
 		frame = new JFrame("THE GAME");
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new GridLayout(30,80));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         button = new JButton("Submit");
         frame.getContentPane().add(button);
         field = new JTextField(20);
         frame.getContentPane().add(field);
         button.addActionListener(new Listener());
-        frame.setSize(400, 90);
+        frame.setSize(960,2560);
         frame.setVisible(true);
 	}
 	public static void main(String[] args) {
 		GUI gui = new GUI();
-	}
-	private class Listener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(e.getSource() == button) {
-	            String text = field.getText();
-	            String xD = "";
-	            int dong = (int) (Math.pow(17,text.hashCode())%73);
-	            int hue = new Random(text.hashCode()).nextInt();
-	            for (char c:text.toCharArray()){
-		            int hash = xD.hashCode();
-	            	xD+=(c>>dong)^hash^hue;
-	            }
-	            if (xD.equals("15339895501326031979481851388165400689-1158800379192249064")) {
-	            	JOptionPane.showMessageDialog(frame, "You win");
-	            } else {
-	            	JOptionPane.showMessageDialog(frame, "Naw");
-				}
-	        }
-		}
-		
 	}
 }
