7ec75881738adbb3ed8e04bf87054c8676bae9ea
Firebolt1176 <Robert@20.9.16.101>
2014-06-03 09:14:20 -0700

    Wrote specific types of equipment

---
+++ APCSGit/src/source/EquipData.java
@@ -0,0 +1,50 @@
+package source;
+
+import source.ItemEquipable.Armor;
+import source.ItemEquipable.Helm;
+import source.ItemEquipable.Leggings;
+import source.ItemEquipable.oneHandWeapon;
+import source.ItemEquipable.shield;
+import source.ItemEquipable.twoHandWeapon;
+
+//maxAtt = 12;
+//maxDef = 12;
+public class EquipData {
+	
+	ItemEquipable dagger = new ItemEquipable.oneHandWeapon("Dagger",1);
+	ItemEquipable shortsword = new ItemEquipable.oneHandWeapon("Shortsword", 2);
+	ItemEquipable rapier = new ItemEquipable.oneHandWeapon("Rapier",3);
+	ItemEquipable broadsword = new ItemEquipable.oneHandWeapon("Broadsword",4);
+	ItemEquipable falchion = new ItemEquipable.oneHandWeapon("Falchion",5);
+	ItemEquipable scimitar = new ItemEquipable.oneHandWeapon("Scimitar",6);
+	
+	ItemEquipable zweihander = new ItemEquipable.twoHandWeapon("Zweihander",8);
+	ItemEquipable flamberge = new ItemEquipable.twoHandWeapon("Flamberge",10);
+	ItemEquipable claymore = new ItemEquipable.twoHandWeapon("Claymore",12);
+	
+	ItemEquipable cuttingBoard = new ItemEquipable.shield("Wooden Shield", 1);
+	ItemEquipable potLid = new ItemEquipable.shield("Pot Lid",2);
+	ItemEquipable bronzeBlocker = new ItemEquipable.shield("Bronze Blocker",3);
+	ItemEquipable ironShield = new ItemEquipable.shield("Iron Shield",4);
+	
+	ItemEquipable clothHat = new ItemEquipable.Helm("Cloth Hat",1);
+	ItemEquipable leatherHelm = new ItemEquipable.Helm("Leather Helm",2);
+	ItemEquipable kettleHat = new ItemEquipable.Helm("Kettle Hat",3);
+	ItemEquipable medHelm = new ItemEquipable.Helm("Medium Helm",4);
+	ItemEquipable fullHelm = new ItemEquipable.Helm("Full Helm",5);
+	
+	ItemEquipable clothRags = new ItemEquipable.Armor("Cloth Rags", 1);
+	ItemEquipable leatherWrap = new ItemEquipable.Armor("Leather Wrappings",2);
+	ItemEquipable reinforcedLeather = new ItemEquipable.Armor("Reinforced Leather",3);
+	ItemEquipable chainmail = new ItemEquipable.Armor("Chainmail",4);
+	ItemEquipable ghettoPlate = new ItemEquipable.Armor("Ghetto Plate",5);
+	ItemEquipable mithrilPlate = new ItemEquipable.Armor("Mithril Plate",6);
+			
+	ItemEquipable medievalSweats = new ItemEquipable.Leggings("Medieval Sweats",1);
+	ItemEquipable CodPieceSweats = new ItemEquipable.Leggings("Codpiece Sweats",2);
+	ItemEquipable YeOldeLevis = new ItemEquipable.Leggings("Ye Olde Levi's",3);
+	
+	
+
+
+}
