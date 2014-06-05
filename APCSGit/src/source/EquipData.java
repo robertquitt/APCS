a7043a8c37dc7d4abaa7c1c9020ebc82c147fc8e
Firebolt1176 <Robert@20.9.16.101>
2014-06-05 08:26:31 -0700

    Made all equipdata items static; added cloning functionality

--- APCSGit/src/source/EquipData.java
+++ APCSGit/src/source/EquipData.java
@@ -4,38 +4,38 @@ package source;
 //maxDef = 12;
 public class EquipData {
 	
-	ItemEquipable dagger = new ItemEquipable.oneHandWeapon("Dagger",1);
-	ItemEquipable shortsword = new ItemEquipable.oneHandWeapon("Shortsword", 2);
-	ItemEquipable rapier = new ItemEquipable.oneHandWeapon("Rapier",3);
-	ItemEquipable broadsword = new ItemEquipable.oneHandWeapon("Broadsword",4);
-	ItemEquipable falchion = new ItemEquipable.oneHandWeapon("Falchion",5);
-	ItemEquipable scimitar = new ItemEquipable.oneHandWeapon("Scimitar",6);
+	static ItemEquipable dagger = new ItemEquipable.oneHandWeapon("Dagger",1);
+	static ItemEquipable shortsword = new ItemEquipable.oneHandWeapon("Shortsword", 2);
+	static ItemEquipable rapier = new ItemEquipable.oneHandWeapon("Rapier",3);
+	static ItemEquipable broadsword = new ItemEquipable.oneHandWeapon("Broadsword",4);
+	static ItemEquipable falchion = new ItemEquipable.oneHandWeapon("Falchion",5);
+	static ItemEquipable scimitar = new ItemEquipable.oneHandWeapon("Scimitar",6);
 	
-	ItemEquipable zweihander = new ItemEquipable.twoHandWeapon("Zweihander",8);
-	ItemEquipable flamberge = new ItemEquipable.twoHandWeapon("Flamberge",10);
-	ItemEquipable claymore = new ItemEquipable.twoHandWeapon("Claymore",12);
+	static ItemEquipable zweihander = new ItemEquipable.twoHandWeapon("Zweihander",8);
+	static ItemEquipable flamberge = new ItemEquipable.twoHandWeapon("Flamberge",10);
+	static ItemEquipable claymore = new ItemEquipable.twoHandWeapon("Claymore",12);
 	
-	ItemEquipable cuttingBoard = new ItemEquipable.shield("Wooden Shield", 1);
-	ItemEquipable potLid = new ItemEquipable.shield("Pot Lid",2);
-	ItemEquipable bronzeBuckler = new ItemEquipable.shield("Bronze Buckler",3);
-	ItemEquipable ironShield = new ItemEquipable.shield("Iron Shield",4);
+	static ItemEquipable cuttingBoard = new ItemEquipable.shield("Wooden Shield", 1);
+	static ItemEquipable potLid = new ItemEquipable.shield("Pot Lid",2);
+	static ItemEquipable bronzeBuckler = new ItemEquipable.shield("Bronze Buckler",3);
+	static ItemEquipable ironShield = new ItemEquipable.shield("Iron Shield",4);
 	
-	ItemEquipable clothHat = new ItemEquipable.Helm("Cloth Hat",1);
-	ItemEquipable leatherHelm = new ItemEquipable.Helm("Leather Helm",2);
-	ItemEquipable kettleHat = new ItemEquipable.Helm("Kettle Hat",3);
-	ItemEquipable medHelm = new ItemEquipable.Helm("Medium Helm",4);
-	ItemEquipable fullHelm = new ItemEquipable.Helm("Full Helm",5);
+	static ItemEquipable clothHat = new ItemEquipable.Helm("Cloth Hat",1);
+	static ItemEquipable leatherHelm = new ItemEquipable.Helm("Leather Helm",2);
+	static ItemEquipable kettleHat = new ItemEquipable.Helm("Kettle Hat",3);
+	static ItemEquipable medHelm = new ItemEquipable.Helm("Medium Helm",4);
+	static ItemEquipable fullHelm = new ItemEquipable.Helm("Full Helm",5);
 	
-	ItemEquipable clothRags = new ItemEquipable.Armor("Cloth Rags", 1);
-	ItemEquipable leatherWrap = new ItemEquipable.Armor("Leather Wrappings",2);
-	ItemEquipable reinforcedLeather = new ItemEquipable.Armor("Reinforced Leather",3);
-	ItemEquipable chainmail = new ItemEquipable.Armor("Chainmail",4);
-	ItemEquipable ghettoPlate = new ItemEquipable.Armor("Ghetto Plate",5);
-	ItemEquipable mithrilPlate = new ItemEquipable.Armor("Mithril Plate",6);
+	static ItemEquipable clothRags = new ItemEquipable.Armor("Cloth Rags", 1);
+	static ItemEquipable leatherWrap = new ItemEquipable.Armor("Leather Wrappings",2);
+	static ItemEquipable reinforcedLeather = new ItemEquipable.Armor("Reinforced Leather",3);
+	static ItemEquipable chainmail = new ItemEquipable.Armor("Chainmail",4);
+	static ItemEquipable ghettoPlate = new ItemEquipable.Armor("Ghetto Plate",5);
+	static ItemEquipable mithrilPlate = new ItemEquipable.Armor("Mithril Plate",6);
 			
-	ItemEquipable medievalSweats = new ItemEquipable.Leggings("Medieval Sweats",1);
-	ItemEquipable CodPieceSweats = new ItemEquipable.Leggings("Codpiece Sweats",2);
-	ItemEquipable YeOldeLevis = new ItemEquipable.Leggings("Ye Olde Levi's",3);
+	static ItemEquipable medievalSweats = new ItemEquipable.Leggings("Medieval Sweats",1);
+	static ItemEquipable CodPieceSweats = new ItemEquipable.Leggings("Codpiece Sweats",2);
+	static ItemEquipable YeOldeLevis = new ItemEquipable.Leggings("Ye Olde Levi's",3);
 	
 	
 
